7fca62ac06552fa999119ad6b36c6efd71f7a9c1
szadarozhny <szadarozhny@insoftgroup.com>
2019-08-20 16:00:09 +0300

    Reimbursements API tests

---
+++ test/suites/reimbursements.test.js
@@ -0,0 +1,34 @@
+const fetch = require('cross-fetch');
+const reimbursements = require('../../lib/api/reimbursements');
+
+const Api = require('../../lib/services/api');
+
+describe('reimbursements API', () => {
+  const reimbursementId = 'sdfgsdg';
+
+  beforeEach(() => {
+    fetch.mockClear();
+  });
+
+  describe('list', () => {
+    it('should call fetch with appropriate params', async () => {
+      const response = reimbursements.list({ page: 2 });
+
+      expect(fetch).toHaveBeenCalledWith(`${Api._apiUrl}/reimbursements?page=2`, {
+        method: 'GET',
+        headers: expect.any(Object),
+      });
+    });
+  });
+
+  describe('retrieve', () => {
+    it('should call fetch with appropriate params', async () => {
+      const response = reimbursements.retrieve(reimbursementId);
+
+      expect(fetch).toHaveBeenCalledWith(`${Api._apiUrl}/reimbursements/${reimbursementId}?`, {
+        method: 'GET',
+        headers: expect.any(Object),
+      });
+    });
+  });
+});
