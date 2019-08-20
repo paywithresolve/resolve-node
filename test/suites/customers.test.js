d1d8aff4f17472fefc74e3c3c24399a3f15e9d7d
szadarozhny <szadarozhny@insoftgroup.com>
2019-08-20 15:58:57 +0300

    Customers API tests

---
+++ test/suites/customers.test.js
@@ -0,0 +1,34 @@
+const fetch = require('cross-fetch');
+const customers = require('../../lib/api/customers');
+
+const Api = require('../../lib/services/api');
+
+describe('Customers API', () => {
+  const customerId = 'sdfgsdg';
+
+  beforeEach(() => {
+    fetch.mockClear();
+  });
+
+  describe('list', () => {
+    it('should call fetch with appropriate params', async () => {
+      const response = customers.list({ page: 2 });
+
+      expect(fetch).toHaveBeenCalledWith(`${Api._apiUrl}/customers?page=2`, {
+        method: 'GET',
+        headers: expect.any(Object),
+      });
+    });
+  });
+
+  describe('retrieve', () => {
+    it('should call fetch with appropriate params', async () => {
+      const response = customers.retrieve(customerId);
+
+      expect(fetch).toHaveBeenCalledWith(`${Api._apiUrl}/customers/${customerId}?`, {
+        method: 'GET',
+        headers: expect.any(Object),
+      });
+    });
+  });
+});
