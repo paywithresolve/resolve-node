f0ec312136b0936da680345e2fd9ef3747207846
szadarozhny <szadarozhny@insoftgroup.com>
2019-08-20 15:59:08 +0300

    Events API tests

---
+++ test/suites/events.test.js
@@ -0,0 +1,34 @@
+const fetch = require('cross-fetch');
+const events = require('../../lib/api/events');
+
+const Api = require('../../lib/services/api');
+
+describe('Events API', () => {
+  const eventId = 'sdfgsdg';
+
+  beforeEach(() => {
+    fetch.mockClear();
+  });
+
+  describe('list', () => {
+    it('should call fetch with appropriate params', async () => {
+      const response = events.list({ page: 2 });
+
+      expect(fetch).toHaveBeenCalledWith(`${Api._apiUrl}/events?page=2`, {
+        method: 'GET',
+        headers: expect.any(Object),
+      });
+    });
+  });
+
+  describe('retrieve', () => {
+    it('should call fetch with appropriate params', async () => {
+      const response = events.retrieve(eventId);
+
+      expect(fetch).toHaveBeenCalledWith(`${Api._apiUrl}/events/${eventId}?`, {
+        method: 'GET',
+        headers: expect.any(Object),
+      });
+    });
+  });
+});
